26cafdcd53cd9f93df0757ce5370720d9cb7a78e
CharlyLuna <cluna10@ucol.mx>
2023-07-26 19:40:13 -0600

    Create Tag component and change style in Markup

--- src/components/ExpMarkup.jsx
+++ src/components/ExpMarkup.jsx
@@ -1,12 +1,22 @@
+import { Tag } from './Tag'
+
 export const ExpMarkup = ({ title, date, tags, description }) => {
   return (
     <div className='flex flex-col md:flex-row gap-4 relative group'>
-      <div className='absolute -inset-x-4 -inset-y-4 z-0 opacity-50 lg:group-hover:bg-primary-red lg:group-hover:shadow-[inset_0_1px_var(--primary-red-200)] lg:drop-shadow-xl transition-all duration-300 rounded-md' />
+      <div className='absolute -inset-x-4 -inset-y-4 z-0 opacity-50 lg:group-hover:bg-black/20 lg:group-hover:shadow-[inset_0_1px_var(--primary-red-200)] lg:drop-shadow-xl transition-all duration-300 rounded-md' />
       <header className='text-sm mb-2 mt-1 z-10 min-w-[180px] text-[#e0e0e0] font-light'>{date}</header>
       <div className='flex flex-col gap-2 z-10'>
         <h2 className='text-lg font-semibold'>{title}</h2>
         <p className='text-light-red'>{description}</p>
-        <ul className='mt-2'>TAGS</ul>
+        <ul className='mt-2 flex gap-2'>
+          {
+            tags?.map((tag, index) => (
+              <li key={`#${tag}_${index}`}>
+                <Tag title={tag} />
+              </li>
+            ))
+          }
+        </ul>
       </div>
     </div>
   )
---
+++ src/components/Tag.jsx
@@ -0,0 +1,7 @@
+export const Tag = ({ title }) => {
+  return (
+    <div className='py-2 px-4 bg-primary-red-200 w-fit rounded-full text-center'>
+      <p className='text-sm'>{title}</p>
+    </div>
+  )
+}
