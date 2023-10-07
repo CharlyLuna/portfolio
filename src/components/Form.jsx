62584919f67d5ae485ce25a0842727bf0af3af41
CharlyLuna <cluna10@ucol.mx>
2023-10-06 21:37:40 -0600

    Implement send mail functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
 
+const SERVICE_ID = import.meta.env.VITE_SERVICE_ID
+const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID
+const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY
 export const Form = () => {
+  const form = useRef()
+
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+      .then((result) => {
+        console.log(result.text)
+        form.current.reset()
+      }, (error) => {
+        console.log(error.text)
+      })
   }
 
   return (
-    <form onSubmit={handleSubmit} className='flex flex-col gap-2 text-secondary shadow-lg bg-[#eee] py-8 px-4 w-[80%] md:w-[50%] max-w-[450px]'>
-      <label htmlFor='user_name'>Name</label>
-      <input className='p-2 rounded-sm ' type='text' name='user_name' />
-      <label htmlFor='user_email'>Email</label>
-      <input className='p-2 rounded-sm ' type='text' name='user_email' />
+    <form ref={form} onSubmit={handleSubmit} className='flex flex-col gap-2 text-secondary shadow-lg bg-[#eee] py-8 px-4 w-[80%] md:w-[50%] max-w-[450px]'>
+      <label htmlFor='name'>Name</label>
+      <input required className='p-2 rounded-sm ' type='text' name='name' />
+      <label htmlFor='email'>Email</label>
+      <input required className='p-2 rounded-sm ' type='email' name='email' />
       <label htmlFor='message'>Message</label>
-      <textarea className='p-2 rounded-sm h-[100px] ' type='text' name='message' />
+      <textarea required className='p-2 rounded-sm h-[100px] ' type='text' name='message' />
       <button type='submit' className='bg-primary text-[#f5f5f5] mt-4 w-fit py-2 px-6 rounded-sm font-semibold hover:scale-105 hover:bg-accent transition-all duration-300 ease-in-out'>Send</button>
     </form>
   )
