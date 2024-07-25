8d63a39dd4d10510077539f4ccf3a3ac59e224be
CharlyLuna <cluna10@ucol.mx>
2024-07-24 22:03:10 -0600

    Change hero image styles

--- src/components/sections/hero/HeroBox.jsx
+++ src/components/sections/hero/HeroBox.jsx
@@ -12,9 +12,7 @@ export const HeroBox = () => {
           <p className='text-sm md:text-base text-secondary sm:w-[80%] max-lg:self-center pt-2'>Focused on Front-End development with React and Next.js. Currently learning about AWS and different technologies, I love to get involve in new projects and take new challengues.</p>
           <a href='/CarlosLuna-CV.pdf' download='carlos-luna-cv' className='bg-primary mt-4 w-fit self-center lg:self-start p-2 rounded-sm font-semibold hover:scale-105 hover:bg-red-800 transition-all duration-300 ease-in-out'>Download CV</a>
         </div>
-        <div className='p-2 border border-secondary rounded-full'>
-          <img className='rounded-full h-60 lg:h-80 xl:h-96 w-auto object-cover' src='/ai-self-pic.png' alt='profile picture' />
-        </div>
+        <img className='rounded-full h-60 lg:h-80 xl:h-96 w-auto object-cover border-2 border-secondary shadow-xl' src='/ai-self-pic.png' alt='profile picture' />
       </div>
 
       <div className='absolute left-8 lg:left-28 bottom-5 flex gap-4 text-slate-800'>
